8b955e40fce2af5662be25fd1e5a0197e9daa5bd
Kevin Barreda <kbarreda.waytech@winempresas.pe>
2026-02-01 21:41:35 -0500

    cambios de arquitectura

---
+++ Main_run.py
@@ -0,0 +1,16 @@
+from Main.BusinessLogic import Process
+import os
+# Main
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ruta_excel = os.path.join(
+    BASE_DIR,  
+    "Main",
+    "Grifos",
+    "BRASIL",
+    "21.01.26",
+    "PARTE DIARIO - SIGES- 3ENERO- BRASIL.xlsx"
+)
+Process.def_Leer_parte_diario("Main/Grifos/brasil/21.01.26/PARTE DIARIO -SIGES- 3ENERO- BRASIL.xlsx","21.01.26")
+
+# Fecha_Procesar = Process.obtener_fecha_actual()
+# print(Fecha_Procesar)
