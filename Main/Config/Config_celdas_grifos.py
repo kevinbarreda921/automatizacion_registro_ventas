178d6741fa9cdb33e6ee80224b5766279cf1c755
kevin <kbarreda.waytech@gmail.com>
2026-02-12 17:56:58 -0500

    configurar write

--- Main/Config/Config_celdas_grifos.py
+++ Main/Config/Config_celdas_grifos.py
@@ -19,17 +19,17 @@ config_celdas_grifos = {
         {'Fila': '01. PUENTE P.',  'Dato': 'Hoja_registro_ventas'},
         {'Fila': 'C',  'Dato': 'Total_venta_acumulada'},
         {'Fila': 'D',  'Dato': 'Venta_GPL'},
-        {'Fila': 'E',  'Dato': 'Venta_GNV'},
-        {'Fila': 'F',  'Dato': 'Recaudo_Cofide_GNV'},
-        {'Fila': 'AO', 'Dato': 'Total_Tarjeta_de_Credito_Liquidos'},
-        {'Fila': 'AP', 'Dato': 'Total_Tarjeta_de_Credito_GLP'},
-        {'Fila': 'AQ', 'Dato': 'Total_Tarjeta_de_Credito_GNV'},
-        {'Fila': 'BY', 'Dato': 'Gastos'},
-        {'Fila': 'AY', 'Dato': 'Ventas_con_transferencia'},
-        {'Fila': 'AX', 'Dato': 'Hermes_monto_liquido'},
-        {'Fila': 'BD', 'Dato': 'Hermes_monto_GLP'},
-        {'Fila': 'BE', 'Dato': 'Hermes_monto_GNV1'},
-        {'Fila': 'BF', 'Dato': 'Hermes_monto_GNV2'}
+        {'Fila': '',  'Dato': 'Venta_GNV'},
+        {'Fila': '',  'Dato': 'Recaudo_Cofide_GNV'},
+        {'Fila': 'AK', 'Dato': 'Total_Tarjeta_de_Credito_Liquidos'},
+        {'Fila': 'AL', 'Dato': 'Total_Tarjeta_de_Credito_GLP'},
+        {'Fila': '', 'Dato': 'Total_Tarjeta_de_Credito_GNV'},
+        {'Fila': 'BN', 'Dato': 'Gastos'},
+        {'Fila': 'AV', 'Dato': 'Ventas_con_transferencia'},
+        {'Fila': 'AT', 'Dato': 'Hermes_monto_liquido'},
+        {'Fila': 'BA', 'Dato': 'Hermes_monto_GLP'},
+        {'Fila': '', 'Dato': 'Hermes_monto_GNV1'},
+        {'Fila': '', 'Dato': 'Hermes_monto_GNV2'}
     ]
     
 }
